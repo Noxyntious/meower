9b2692bc748a6aa99cfbf23f7b5f13a2b6c676bf
aemogie <54412618+aemogie@users.noreply.github.com>
2024-04-11 16:29:05 +0530

    refactor: some opinionated changes

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,40 @@
 // this program was a mistake. what am i doing
 use rand::Rng;
 use std::env;
-use std::process;
+use std::process::ExitCode;
 
-fn main() {
-    let meows = vec!["meow", "mrrrow", "mrrrp", "nyaaa", "nya", "mew", "meowwww"];
-    let args: Vec<_> = env::args().collect();
-    if args.len() <= 1 {
-        println!("meower help:\n\nusage: meower <amount>\nexample: meower 10");
-        process::exit(0x1);
-    }
-    let amount = args[1].parse::<i32>().unwrap()-1;
-    let mut i: i32 = 0;
-    let mut prevnum = 9999;
-    while i <= amount {
+fn main() -> ExitCode {
+    let meows = ["meow", "mrrrow", "mrrrp", "nyaaa", "nya", "mew", "meowwww"];
+    let amount = if let Some(amount) = env::args().skip(1).next() {
+        amount.parse::<usize>().unwrap()
+    } else {
+        println!(
+            "meower help:\n\
+            \n\
+            usage: meower <amount>\n\
+            example: meower 10"
+        );
+        return ExitCode::FAILURE;
+    };
+    let mut prevnum: Option<usize> = None;
+    for _ in 0..amount {
         let mut num = rand::thread_rng().gen_range(0..meows.len());
-        if num == prevnum {
-            if num > 3 {
-                num = num-1
-            } else if num < 3 {
-                num = num+1
+        if let Some(prevnum) = prevnum {
+            if num == prevnum {
+                num = if num > (meows.len() / 2) {
+                    num - 1
+                } else {
+                    num + 1
+                };
             }
+            print!(" {}", meows[num]);
+        } else {
+            print!("{}", meows[num]);
         }
-        print!("{} ", meows[num]);
-        prevnum = num;
-        i = i + 1;
+        prevnum = Some(num);
     }
-    let colonthree = rand::thread_rng().gen_range(0..10);
-    if colonthree >= 5 {
-        print!(":3");
+    if rand::thread_rng().gen_bool(0.5) {
+        print!(" :3");
     }
+    return ExitCode::SUCCESS;
 }
